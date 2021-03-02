bb1d1eb0dce8115e6ba616bd870651a83a6e509c
NikolasVieira <nikmap@hotmail.com>
2021-03-02 08:29:10 -0300

    aula 3 continuação
    continuação da aula 3

--- agenda/resources/views/pessoa/index.blade.php
+++ agenda/resources/views/pessoa/index.blade.php
@@ -3,24 +3,31 @@
 @section('content')
 
 <div class="container">
+
+    <h2>Agenda</h2>
+    <hr/>
+    <a href="{{ url('pessoa/create') }}" class="btn btn-sucess">Novo Contato</a>
+    <br/><br/>
+
     <table class="table">
-        <thead>
+        <thead class="thead-dark">
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Sobrenome</th>
+                <th scope="col">Numero</th>
+                <th scope="col">Opções</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td scope="row">1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <td>Nikolas</td>
+                <td>Raposo</td>
+                <td>(17)98828-7775</td>
+                <td></td>
             </tr>
         </tbody>
     </table>
+
 </div>
 
 @endsection
